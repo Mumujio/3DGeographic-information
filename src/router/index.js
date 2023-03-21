8d8f241c5b1417f150aeced2dc43c00d8a8aa15d
linzhuo <qpzm7913..@>
2023-03-21 22:06:33 +0800

    3.21

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter,createWebHashHistory} from 'vue-router'
 const routes = [
   {
     path: '/index',
@@ -57,7 +57,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) { 
     // 始终滚动到顶部
